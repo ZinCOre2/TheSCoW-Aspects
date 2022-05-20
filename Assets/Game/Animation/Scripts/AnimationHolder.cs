77246eb50c28b05f497008b218f69ee1db86c0af
ZinCOre2 <98097347+ZinCOre2@users.noreply.github.com>
2022-05-20 12:02:08 +0300

    fork-change-project-structure

---
+++ Assets/Game/Animation/Scripts/AnimationHolder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHolder : MonoBehaviour
+{
+    public Animation Animation;
+    public AnimationType Type;
+    public float CustomDelay;
+}
---
+++ Assets/Game/Animation/Scripts/AnimationType.cs
@@ -0,0 +1,12 @@
+
+public enum AnimationType
+{
+    Idle,
+    Move,
+    CastSpell,
+    CastSpell2,
+    Attack1,
+    Attack2,
+    TakeDamage,
+    Die
+}
